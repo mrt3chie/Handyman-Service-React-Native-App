9278e2752ebc1617847c26a109a3c7896566f52e
Rageshnithin <72620986+mrt3chie@users.noreply.github.com>
2023-09-22 11:30:04 +0530

    Update confignew.js

--- confignew.js
+++ confignew.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB4ScaEIp9GS40MH5iulM0_u8xTCmszyl0",
-    authDomain: "handyman-application.firebaseapp.com",
-    projectId: "handyman-application",
-    storageBucket: "handyman-application.appspot.com",
-    messagingSenderId: "615134807170",
-    appId: "1:615134807170:web:444e0fff05c5b9f39f609b",
-    measurementId: "G-83Y1N6ZTHN"
+    apiKey: "YOUR_INFO",
+    authDomain: "YOUR_INFO",
+    projectId: "YOUR_INFO",
+    storageBucket: "YOUR_INFO",
+    messagingSenderId: "YOUR_INFO",
+    appId: "YOUR_INFO",
+    measurementId: "YOUR_INFO"
   };
   
 if(!firebase.apps.length){
